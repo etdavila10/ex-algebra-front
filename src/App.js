c9d5ed2ecd6743ea6ab5011032608c5c733433b5
Eduardo Torres Davila <etdavila10@gmail.com>
2022-09-05 12:27:25 -0500

    Initial SetUp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './sass/index.scss';
+
+const App = () => (
+  <div>
+    <h1>Hello App</h1>
+    <h2>This is a subheading</h2>
+    <h3>This is a subsubheading</h3>
+    <code>This is a code syntax</code>
+  </div>
+);
+
+export default App;
